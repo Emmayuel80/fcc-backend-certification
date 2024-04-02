e1eb1425cdf464be1eebb2ba80ec2c1fb9102a12
Silvestre Emmanuel <silvestre.ramirez@wundertec.com>
2024-04-02 17:27:17 -0600

    fixed a type conversion error

--- routes/timestamp-microservice/index.js
+++ routes/timestamp-microservice/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/:date", (req, res) => {
+router.get("/:date?", (req, res) => {
   const date = req.params.date;
-  const dateObject = new Date(date);
+  const dateObject = new Date(Number(date));
   const unix = dateObject.getTime();
   const utc = dateObject.toUTCString();
+  console.log(dateObject);
   if (utc === "Invalid Date") {
     res.json({ error: "Invalid Date" });
   } else {
