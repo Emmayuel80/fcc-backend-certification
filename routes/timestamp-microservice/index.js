5432864d15ce596edd1ff72ad823e7f770d95317
Silvestre Emmanuel <silvestre.ramirez@wundertec.com>
2024-04-02 17:47:05 -0600

    if no date parameter then return current date

--- routes/timestamp-microservice/index.js
+++ routes/timestamp-microservice/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 router.get("/:date?", (req, res) => {
   const date = req.params.date;
-
+  if (!date) {
+    const dateObject = new Date();
+    const unix = dateObject.getTime();
+    const utc = dateObject.toUTCString();
+    return res.json({ unix, utc });
+  }
   const dateObject = new Date(isNaN(date) ? date : Number(date));
   const unix = dateObject.getTime();
   const utc = dateObject.toUTCString();
