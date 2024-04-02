a8b9c8e4ba8d95346ae42620bb389aaf8233d68a
Silvestre Emmanuel <silvestre.ramirez@wundertec.com>
2024-04-02 14:56:55 -0600

    added request header api routes

---
+++ routes/requestheaderparser-microservice/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/whoami", (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+  res.json({ ipaddress, language, software });
+});
+
+module.exports = router;
