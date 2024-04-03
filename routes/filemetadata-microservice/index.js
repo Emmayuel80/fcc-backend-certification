93963650429acdef151ada74bfa5faf55a79c157
Silvestre Emmanuel <silvestre.ramirez@wundertec.com>
2024-04-02 18:40:17 -0600

    fixed file form name

--- routes/filemetadata-microservice/index.js
+++ routes/filemetadata-microservice/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-router.post("/", upload.single("file"), (req, res) => {
+router.post("/", upload.single("upfile"), (req, res) => {
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
